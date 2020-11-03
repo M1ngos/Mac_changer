2c342b0891b02c12547a96590213ae4bfcbf0eb3
M1ngos <mingoemanuel07@gmail.com>
2020-11-03 19:36:36 +0200

    Initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Mac_changer',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='G4Z studios',
+    author_email='Domingos.emanuel.mavie@gmail.com',
+    description=''
+)
